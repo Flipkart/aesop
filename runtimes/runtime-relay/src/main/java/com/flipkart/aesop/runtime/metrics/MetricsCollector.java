49fe6f857499f0005dd79bd9e420fbe0a70f9a8f
Regunath B <regunathb@gmail.com>
2015-03-24 19:02:15 +0530

    Added commentts to code

--- runtimes/runtime-relay/src/main/java/com/flipkart/aesop/runtime/metrics/MetricsCollector.java
+++ runtimes/runtime-relay/src/main/java/com/flipkart/aesop/runtime/metrics/MetricsCollector.java
@@ -1,10 +1,19 @@
+/*
+ * Copyright 2012-2015, the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.flipkart.aesop.runtime.metrics;
-
-import com.flipkart.aesop.runtime.relay.DefaultRelay;
-import com.linkedin.databus.container.netty.HttpRelay;
-import com.linkedin.databus.core.monitoring.mbean.DbusEventsTotalStats;
-import com.linkedin.databus2.core.container.monitoring.mbean.DbusHttpTotalStats;
-import org.codehaus.jackson.map.ObjectMapper;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -12,7 +21,14 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+import org.codehaus.jackson.map.ObjectMapper;
+
+import com.flipkart.aesop.runtime.relay.DefaultRelay;
+import com.linkedin.databus.core.monitoring.mbean.DbusEventsTotalStats;
+import com.linkedin.databus2.core.container.monitoring.mbean.DbusHttpTotalStats;
+
 /**
+ * Periodically collects relay statistics for producers and consumers and serializes it into a JSON representation. 
  * @author kartikbu
  * @created 09/06/14
  */
@@ -98,7 +114,7 @@ public class MetricsCollector {
     }
 
     /**
-     * Encoder class which encodes (JSON) registry intsance.
+     * Encoder class which JSON encodes metrics data.
      */
     public class Encoder implements Runnable {
 
--- runtimes/runtime-relay/src/main/java/com/flipkart/aesop/runtime/producer/ProducerEventBuffer.java
+++ runtimes/runtime-relay/src/main/java/com/flipkart/aesop/runtime/producer/ProducerEventBuffer.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012-2015, the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.flipkart.aesop.runtime.producer;
 
 import com.flipkart.aesop.runtime.metrics.MetricsCollector;
@@ -5,96 +20,151 @@ import com.linkedin.databus.core.*;
 import com.linkedin.databus.core.monitoring.mbean.DbusEventsStatisticsCollector;
 
 /**
+ * Wrapper for the Databus event buffer that uses a {@link MetricsCollector} to tracking producer SCNs
  * @author kartikbu
  * @created 10/07/14
  */
 public class ProducerEventBuffer implements DbusEventBufferAppendable {
 
+	/** The producer name*/
     private String producerName;
 
+    /** The event buffer that is wrapped by this event buffer*/
     private DbusEventBufferAppendable eventBuffer;
 
+    /** The metrics collector for tracking producer SCNs*/
     private MetricsCollector collector;
 
+    /**
+     * Constructor for this class
+     * @param producerName the name of the producer
+     * @param eventBuffer the event buffer that holds event data
+     * @param collector the metrics collector instance
+     */
     public ProducerEventBuffer(String producerName, DbusEventBufferAppendable eventBuffer, MetricsCollector collector) {
         this.producerName = producerName;
         this.eventBuffer = eventBuffer;
         this.collector = collector;
     }
 
-    @Override
+    /**
+     * Forwards the call to the underlying event buffer
+     * @see com.linkedin.databus.core.DbusEventBufferAppendable#start(long)
+     */
     public void start(long l) {
         eventBuffer.start(l);
     }
 
-    @Override
+    /**
+     * Forwards the call to the underlying event buffer
+     * @see com.linkedin.databus.core.DbusEventBufferAppendable#startEvents()
+     */
     public void startEvents() {
         eventBuffer.startEvents();
     }
 
-    @Override
+    /**
+     * Forwards the call to the underlying event buffer
+     * @see com.linkedin.databus.core.DbusEventBufferAppendable#appendEvent(com.linkedin.databus.core.DbusEventKey, long, short, short, long, short, byte[], byte[], boolean, com.linkedin.databus.core.monitoring.mbean.DbusEventsStatisticsCollector)
+     */
     public boolean appendEvent(DbusEventKey dbusEventKey, long l, short i, short i2, long l2, short i3, byte[] bytes, byte[] bytes2, boolean b, DbusEventsStatisticsCollector statisticsCollector) {
         return eventBuffer.appendEvent(dbusEventKey, l, i, i2, l2, i3, bytes, bytes2, b, statisticsCollector);
     }
 
-    @Override
+    /**
+     * Forwards the call to the underlying event buffer
+     * @see com.linkedin.databus.core.DbusEventBufferAppendable#appendEvent(com.linkedin.databus.core.DbusEventKey, short, short, long, short, byte[], byte[], boolean)
+     */
     public boolean appendEvent(DbusEventKey dbusEventKey, short i, short i2, long l, short i3, byte[] bytes, byte[] bytes2, boolean b) {
         return eventBuffer.appendEvent(dbusEventKey, i, i2, l, i3, bytes, bytes2, b);
     }
 
-    @Override
+    /**
+     * Forwards the call to the underlying event buffer
+     * @see com.linkedin.databus.core.DbusEventBufferAppendable#appendEvent(com.linkedin.databus.core.DbusEventKey, short, short, long, short, byte[], byte[], boolean, com.linkedin.databus.core.monitoring.mbean.DbusEventsStatisticsCollector)
+     */
     public boolean appendEvent(DbusEventKey dbusEventKey, short i, short i2, long l, short i3, byte[] bytes, byte[] bytes2, boolean b, DbusEventsStatisticsCollector statisticsCollector) {
         return eventBuffer.appendEvent(dbusEventKey,i,i2,l,i3,bytes,bytes2,b,statisticsCollector);
     }
 
-    @Override
+    /**
+     * Forwards the call to the underlying event buffer
+     * @see com.linkedin.databus.core.DbusEventBufferAppendable#appendEvent(com.linkedin.databus.core.DbusEventKey, short, short, long, short, byte[], byte[], boolean, boolean, com.linkedin.databus.core.monitoring.mbean.DbusEventsStatisticsCollector)
+     */
     public boolean appendEvent(DbusEventKey dbusEventKey, short i, short i2, long l, short i3, byte[] bytes, byte[] bytes2, boolean b, boolean b2, DbusEventsStatisticsCollector statisticsCollector) {
         return eventBuffer.appendEvent(dbusEventKey, i, i2, l, i3, bytes, bytes2, b, b2, statisticsCollector);
     }
 
-    @Override
+    /**
+     * Forwards the call to the underlying event buffer
+     * @see com.linkedin.databus.core.DbusEventBufferAppendable#appendEvent(com.linkedin.databus.core.DbusEventKey, com.linkedin.databus.core.DbusEventInfo, com.linkedin.databus.core.monitoring.mbean.DbusEventsStatisticsCollector)
+     */
     public boolean appendEvent(DbusEventKey dbusEventKey, DbusEventInfo dbusEventInfo, DbusEventsStatisticsCollector statisticsCollector) {
         return eventBuffer.appendEvent(dbusEventKey, dbusEventInfo, statisticsCollector);
     }
 
-    @Override
+    /**
+     * Forwards the call to the underlying event buffer
+     * @see com.linkedin.databus.core.DbusEventBufferAppendable#rollbackEvents()
+     */
     public void rollbackEvents() {
         eventBuffer.rollbackEvents();
     }
 
-    @Override
+    /**
+     * Forwards the call to the underlying event buffer. Also records the producer SCN on the metrics collector
+     * @see com.linkedin.databus.core.DbusEventBufferAppendable#endEvents(boolean, long, boolean, boolean, com.linkedin.databus.core.monitoring.mbean.DbusEventsStatisticsCollector)
+     */
     public void endEvents(boolean b, long l, boolean b2, boolean b3, DbusEventsStatisticsCollector statisticsCollector) {
         eventBuffer.endEvents(b,l,b2,b3,statisticsCollector);
         collector.setProducerSCN(producerName,eventBuffer.lastWrittenScn());
     }
 
-    @Override
+    /**
+     * Forwards the call to the underlying event buffer. Also records the producer SCN on the metrics collector
+     * @see com.linkedin.databus.core.DbusEventBufferAppendable#endEvents(long, com.linkedin.databus.core.monitoring.mbean.DbusEventsStatisticsCollector)
+     */
     public void endEvents(long l, DbusEventsStatisticsCollector statisticsCollector) {
         eventBuffer.endEvents(l, statisticsCollector);
         collector.setProducerSCN(producerName,eventBuffer.lastWrittenScn());
     }
 
-    @Override
+    /**
+     * Forwards the call to the underlying event buffer
+     * @see com.linkedin.databus.core.DbusEventBufferAppendable#empty()
+     */
     public boolean empty() {
         return eventBuffer.empty();
     }
 
-    @Override
+    /**
+     * Forwards the call to the underlying event buffer
+     * @see com.linkedin.databus.core.DbusEventBufferAppendable#getMinScn()
+     */
     public long getMinScn() {
         return eventBuffer.getMinScn();
     }
 
-    @Override
+    /**
+     * Forwards the call to the underlying event buffer
+     * @see com.linkedin.databus.core.DbusEventBufferAppendable#lastWrittenScn()
+     */
     public long lastWrittenScn() {
         return eventBuffer.lastWrittenScn();
     }
 
-    @Override
+    /**
+     * Forwards the call to the underlying event buffer
+     * @see com.linkedin.databus.core.DbusEventBufferAppendable#setStartSCN(long)
+     */
     public void setStartSCN(long l) {
         eventBuffer.setStartSCN(l);
     }
 
-    @Override
+    /**
+     * Forwards the call to the underlying event buffer
+     * @see com.linkedin.databus.core.DbusEventBufferAppendable#getPrevScn()
+     */
     public long getPrevScn() {
         return eventBuffer.getPrevScn();
     }
