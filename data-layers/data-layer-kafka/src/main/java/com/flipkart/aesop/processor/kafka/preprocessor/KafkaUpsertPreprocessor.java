0376bf3eda88a50c27f024c6a775ba749f8f307a
ravindra.yadav <r.yadav@flipkart.com>
2015-04-13 12:07:00 +0530

    made changes in aesop pom to include kafka client

---
+++ data-layers/data-layer-kafka/src/main/java/com/flipkart/aesop/processor/kafka/preprocessor/KafkaUpsertPreprocessor.java
@@ -0,0 +1,9 @@
+package com.flipkart.aesop.processor.kafka.preprocessor;
+
+import com.flipkart.aesop.destinationoperation.UpsertDestinationStoreProcessor;
+
+/**
+ * Created by r.yadav on 10/04/15.
+ */
+public class KafkaUpsertPreprocessor extends UpsertDestinationStoreProcessor {
+}
