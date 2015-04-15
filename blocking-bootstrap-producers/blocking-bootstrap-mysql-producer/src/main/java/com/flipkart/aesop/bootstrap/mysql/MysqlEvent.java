95196cf71813c3e4d1a941fee9ce466507ea5467
ARYA KETAN <arya.ketan@flipkart.com>
2015-04-15 16:45:58 +0530

    Blocking bootstrap producer changes a) Addition of basic UI to track producer SCN b) spring-bootstrap-config made in sync with spring-relay-config.    Removed OpenReplicatorConfig. Used Product registeration list , physical source configs etc c) Support of multiple producer physical host config d) Support for transactions

--- blocking-bootstrap-producers/blocking-bootstrap-mysql-producer/src/main/java/com/flipkart/aesop/bootstrap/mysql/MysqlEvent.java
+++ blocking-bootstrap-producers/blocking-bootstrap-mysql-producer/src/main/java/com/flipkart/aesop/bootstrap/mysql/MysqlEvent.java
@@ -3,9 +3,14 @@ package com.flipkart.aesop.bootstrap.mysql;
 
 import com.flipkart.aesop.event.AbstractEvent;
 
+/* This is a wrapper class for Abstract Event and SCN. This is to track SCNs with each
+ * generated event .
+ */
 public class MysqlEvent
 {
+    /* Associated SCN of the Transformed Source Event */
     private long scn;
+    /* Abstract Event */
     private AbstractEvent abstractEvent;
 
     public MysqlEvent(long scn,AbstractEvent abstractEvent) {
