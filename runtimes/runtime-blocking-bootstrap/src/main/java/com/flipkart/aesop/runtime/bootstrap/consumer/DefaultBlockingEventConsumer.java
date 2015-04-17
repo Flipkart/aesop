a385b812aeca6baeb656d1a9c434681f7bfd9cb9
ARYA KETAN <arya.ketan@flipkart.com>
2015-04-17 21:41:00 +0530

    Setting max Await time for thread termination

--- runtimes/runtime-blocking-bootstrap/src/main/java/com/flipkart/aesop/runtime/bootstrap/consumer/DefaultBlockingEventConsumer.java
+++ runtimes/runtime-blocking-bootstrap/src/main/java/com/flipkart/aesop/runtime/bootstrap/consumer/DefaultBlockingEventConsumer.java
@@ -17,6 +17,7 @@ import com.flipkart.aesop.event.AbstractEvent;
 import com.flipkart.aesop.eventconsumer.AbstractEventConsumer;
 import com.google.common.base.Joiner;
 import com.linkedin.databus2.core.BackoffTimer;
+import org.springframework.beans.factory.InitializingBean;
 import org.trpr.platform.core.impl.logging.LogFactory;
 import org.trpr.platform.core.spi.logging.Logger;
 
@@ -29,58 +30,103 @@ import java.util.concurrent.*;
  * submits the events to the appropriate thread pool.
  * @author nrbafna
  */
-public class DefaultBlockingEventConsumer implements SourceEventConsumer
+public class DefaultBlockingEventConsumer implements SourceEventConsumer,InitializingBean
 {
-	public static final Logger LOGGER = LogFactory.getLogger(DefaultBlockingEventConsumer.class);
+    public static final Logger LOGGER = LogFactory.getLogger(DefaultBlockingEventConsumer.class);
 
-	private final String PRIMARY_KEY_SEPERATOR = ";";
-	private List<ThreadPoolExecutor> executors = new ArrayList<ThreadPoolExecutor>();
-	private final int numberOfPartition;
-	private final AbstractEventConsumer eventConsumer;
-    private final BackoffTimer backoffTimer;
+    private static final long DEFAULT_THREAD_AWAIT_TERMINATION_TIME_IN_SECS = 60;
+    private final String PRIMARY_KEY_SEPERATOR = ";";
+    /** executor pool for parallelizing consumption */
+    private List<ThreadPoolExecutor> executors = new ArrayList<ThreadPoolExecutor>();
+    /** No. Of Partitions to create  */
+    private int numberOfPartition;
+    /* Event Consumer to send events to */
+    private AbstractEventConsumer eventConsumer;
+    /* Back-Off Timer config */
+    private BackoffTimer backoffTimer;
+    /*
+     * Thread Termination Max time. This config will ensure that threads
+     * termination during shut-down is waiting for max these many secs
+     * */
+    private long threadTerminationMaxTime = DEFAULT_THREAD_AWAIT_TERMINATION_TIME_IN_SECS;
+    private int executorQueueSize;
+    private RejectedExecutionHandler rejectedExecutionHandler;
 
-	public DefaultBlockingEventConsumer(int numberOfPartition, int executorQueueSize,
-	        AbstractEventConsumer eventConsumer, RejectedExecutionHandler rejectedExecutionHandler, BackoffTimer backoffTimer)
-	{
-		this.eventConsumer = eventConsumer;
-		this.numberOfPartition = Math.min(numberOfPartition, Runtime.getRuntime().availableProcessors());
+    @Override
+    public void onEvent(AbstractEvent sourceEvent)
+    {
+        /** partition and submit */
+        String primaryKeyValues = Joiner.on(PRIMARY_KEY_SEPERATOR).join(sourceEvent.getPrimaryKeyValues());
+        Integer partitionNumber = ((primaryKeyValues.hashCode() & 0x7fffffff) % numberOfPartition);
+        LOGGER.debug("Partition:" + primaryKeyValues.hashCode() + ":" + partitionNumber);
+        executors.get(partitionNumber).execute(new SourceEventProcessor(sourceEvent, eventConsumer,backoffTimer));
+    }
+
+    public void shutdown()
+    {
+        for (int i = 0; i < numberOfPartition; i++) {
+            executors.get(i).shutdown();
+        }
+
+        try
+        {
+            for (int i = 0; i < numberOfPartition; i++) {
+                executors.get(i).awaitTermination(threadTerminationMaxTime, TimeUnit.NANOSECONDS);
+            }
+        }
+        catch (InterruptedException e)
+        {
+            LOGGER.error("Error while stopping bootstrap consumer", e);
+        }
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception
+    {
+        this.numberOfPartition = Math.min(numberOfPartition, Runtime.getRuntime().availableProcessors());
+        LOGGER.info("numberOfPartition used: " + numberOfPartition);
+        for (int i = 0; i < numberOfPartition; i++)
+        {
+            BlockingQueue<Runnable> queue = new ArrayBlockingQueue<Runnable>(executorQueueSize);
+            executors.add(new ThreadPoolExecutor(1, 1, 0, TimeUnit.MILLISECONDS, queue, rejectedExecutionHandler));
+        }
+    }
+
+    /** Getters and Setters */
+    public AbstractEventConsumer getEventConsumer() {
+        return eventConsumer;
+    }
+    public void setEventConsumer(AbstractEventConsumer eventConsumer) {
+        this.eventConsumer = eventConsumer;
+    }
+    public BackoffTimer getBackoffTimer() {
+        return backoffTimer;
+    }
+    public void setBackoffTimer(BackoffTimer backoffTimer) {
         this.backoffTimer = backoffTimer;
-
-		LOGGER.info("numberOfPartition used: " + numberOfPartition);
-		for (int i = 0; i < numberOfPartition; i++)
-		{
-			BlockingQueue<Runnable> queue = new ArrayBlockingQueue<Runnable>(executorQueueSize);
-			executors.add(new ThreadPoolExecutor(1, 1, 0, TimeUnit.MILLISECONDS, queue, rejectedExecutionHandler));
-		}
-	}
-
-	@Override
-	public void onEvent(AbstractEvent sourceEvent)
-	{
-		/** partition and submit */
-		String primaryKeyValues = Joiner.on(PRIMARY_KEY_SEPERATOR).join(sourceEvent.getPrimaryKeyValues());
-		Integer partitionNumber = ((primaryKeyValues.hashCode() & 0x7fffffff) % numberOfPartition);
-		LOGGER.debug("Partition:" + primaryKeyValues.hashCode() + ":" + partitionNumber);
-		executors.get(partitionNumber).execute(new SourceEventProcessor(sourceEvent, eventConsumer,backoffTimer));
-	}
-
-	public void shutdown()
-	{
-		for (int i = 0; i < numberOfPartition; i++)
-		{
-			executors.get(i).shutdown();
-		}
-
-		try
-		{
-			for (int i = 0; i < numberOfPartition; i++)
-			{
-				executors.get(i).awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
-			}
-		}
-		catch (InterruptedException e)
-		{
-			LOGGER.error("Error while stopping bootstrap consumer", e);
-		}
-	}
+    }
+    public long getThreadTerminationMaxTime() {
+        return threadTerminationMaxTime;
+    }
+    public void setThreadTerminationMaxTime(long threadTerminationMaxTime) {
+        this.threadTerminationMaxTime = threadTerminationMaxTime;
+    }
+    public int getExecutorQueueSize() {
+        return executorQueueSize;
+    }
+    public void setExecutorQueueSize(int executorQueueSize) {
+        this.executorQueueSize = executorQueueSize;
+    }
+    public RejectedExecutionHandler getRejectedExecutionHandler() {
+        return rejectedExecutionHandler;
+    }
+    public void setRejectedExecutionHandler(RejectedExecutionHandler rejectedExecutionHandler) {
+        this.rejectedExecutionHandler = rejectedExecutionHandler;
+    }
+    public int getNumberOfPartition() {
+        return numberOfPartition;
+    }
+    public void setNumberOfPartition(int numberOfPartition) {
+        this.numberOfPartition = numberOfPartition;
+    }
 }
