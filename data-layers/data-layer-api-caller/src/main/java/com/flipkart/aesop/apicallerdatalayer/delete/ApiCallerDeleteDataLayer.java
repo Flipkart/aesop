9c660cdbe83895c3c9f53f43badf94323368f667
Aman Gupta <aman.gupta@flipkart.com>
2015-11-30 18:28:57 +0530

    generalized spring config

--- data-layers/data-layer-api-caller/src/main/java/com/flipkart/aesop/apicallerdatalayer/delete/ApiCallerDeleteDataLayer.java
+++ data-layers/data-layer-api-caller/src/main/java/com/flipkart/aesop/apicallerdatalayer/delete/ApiCallerDeleteDataLayer.java
@@ -2,18 +2,18 @@ package com.flipkart.aesop.apicallerdatalayer.delete;
 
 import com.flipkart.aesop.destinationoperation.DeleteDestinationStoreProcessor;
 import com.flipkart.aesop.event.AbstractEvent;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.linkedin.databus.client.pub.ConsumerCallbackResult;
-import org.json.JSONException;
 import org.json.JSONObject;
 import org.trpr.platform.core.impl.logging.LogFactory;
 import org.trpr.platform.core.spi.logging.Logger;
 
 import java.io.DataOutputStream;
-import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.ProtocolException;
 import java.net.URL;
 import java.util.Iterator;
+import java.util.Map;
 
 /**
  * Created by aman.gupta on 12/08/15.
@@ -31,32 +31,31 @@ public class ApiCallerDeleteDataLayer extends DeleteDestinationStoreProcessor {
     @Override
     protected ConsumerCallbackResult delete(AbstractEvent event) {
         try {
-            JSONObject param = new JSONObject(event.getFieldMapPair());
-            final String USER_AGENT = "Mozilla/5.0";
-            LOGGER.info("Making a post call to url: "+url+" with payload as: "+param.toString());
+            Map<String,Object> eventMap = event.getFieldMapPair();
+            Gson gson = new GsonBuilder().serializeNulls().create();
+            String param = gson.toJson(eventMap);
+            LOGGER.info("Making a post call to url: "+url+" with payload as: "+param);
             HttpURLConnection con = (HttpURLConnection)url.openConnection();
             con.setRequestMethod("POST");
-            con.setRequestProperty("User-Agent", USER_AGENT);
-            con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
             con.setRequestProperty("Content-Type", "application/json");
             con.setRequestProperty("Accept", "application/json");
             Iterator<String> it = headers.keys();
-            //If a header value starts with $, the corresponding value from the payload is used.
+            //If a header value starts with $, the corresponding value from the payload is used. See documentation for details.
             while(it.hasNext()){
                 String header = it.next();
                 String value = headers.get(header).toString();
                 if(value.charAt(0)=='$'){
-                    value = param.get(value.substring(1)).toString();
+                    value = eventMap.get(value.substring(1)).toString();
                 }
                 con.setRequestProperty(header,value);
             }
             con.setDoOutput(true);
             DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-            wr.writeBytes(param.toString());
+            wr.writeBytes(param);
             wr.flush();
             wr.close();
             int responseCode = con.getResponseCode();
-            LOGGER.info("Call successful with response code as "+responseCode+ "for payload: "+param.toString());
+            LOGGER.info("Call successful with response code as "+responseCode+ "for payload: "+param);
             return ConsumerCallbackResult.SUCCESS;
         } catch(Exception e){
             LOGGER.error("Call unsuccessful with error:",e);
